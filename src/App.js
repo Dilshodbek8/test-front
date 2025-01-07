dc2c49c57d3a1c2551d3e94bfa136376c8f76b63
Dilshodbek8 <20020905xd@gmail.com>
2025-01-07 19:33:09 +0500

    starter

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  Collapse,
+  Nav,
+  NavItem,
+  NavLink,
+} from "@material-tailwind/react";
+
+export default function App() {
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => setOpen(!open);
+
+  return (
+    <div className="App">
+      <Navbar className="bg-gray-600">
+        <NavbarBrand>Material Tailwind + CRA</NavbarBrand>
+        <NavbarToggler onClick={handleOpen} />
+        <Collapse open={open}>
+          <Nav>
+            <NavItem>
+              <NavLink href="#" active>
+                Home
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="#">Features</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="#">About</NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+      <footer className="flex items-center justify-center w-full h-24 border-t dark:border-gray-600">
+        <div className="flex items-center justify-center">
+          <p className="text-sm text-gray-600 dark:text-gray-300">
+            Material Tailwind + CRA
+          </p>
+        </div>
+      </footer>
+    </div>
+  );
+}
